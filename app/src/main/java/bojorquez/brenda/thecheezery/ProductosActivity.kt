0a5f90624004481e379b87bee8d63d81cbeacab6
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-02-20 22:16:00 -0700

    Initial commit

---
+++ app/src/main/java/bojorquez/brenda/thecheezery/ProductosActivity.kt
@@ -0,0 +1,11 @@
+package bojorquez.brenda.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+    }
+}
